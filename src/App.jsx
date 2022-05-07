c799ad22823b81e3489c351831a7bbabe7eacb96
Rowan Kh <rowan.kh1@gmail.com>
2022-05-07 19:29:43 +0200

    w: Firebase install

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import "./App.css";
 import "../src/main.scss";
-// import '/g';
 import { Route, Routes } from "react-router-dom";
 import Layout from "./containers/Layout";
 import Index from "./components/Index/Index";
 import Catalog from "./components/Catalog/Catalog";
+import firebase_config from "./server/firebase_config";
+import { initializeApp } from "firebase/app";
+
+
+initializeApp(firebase_config);
+
 
 function App() {
   return (
@@ -13,6 +18,8 @@ function App() {
       <Route element={<Layout />}>
         <Route index element={<Index />} />
         <Route path="catalog" element={<Catalog />} />
+        {/* <Route path="signup" element={<Signup />} /> */}
+        {/* <Route path="login" element={<Login />} /> */}
       </Route>
     </Routes>
   );
