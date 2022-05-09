e6a6c11f87c3b81b98f8f6e61e84458572595c9b
Rowan Kh <rowan.kh1@gmail.com>
2022-05-09 21:27:32 +0200

    w: firebase_config in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import "./App.css";
 import "../src/main.scss";
+// import { app } from "./stores/app";
 import { Route, Routes } from "react-router-dom";
+import { initializeApp } from "firebase/app";
+import firebase_config from "./server/firebase_config";
+
 import Layout from "./containers/Layout";
 import Index from "./components/Index/Index";
 import Catalog from "./components/Catalog/Catalog";
-import firebase_config from "./server/firebase_config";
-import { initializeApp } from "firebase/app";
+import Login from "./components/Login/Login";
+
+  // Firebase 9
+const fb = initializeApp(firebase_config);
 
 
-initializeApp(firebase_config);
+// app.subscribe(() => {});
+
 
 
 function App() {
@@ -18,8 +25,8 @@ function App() {
       <Route element={<Layout />}>
         <Route index element={<Index />} />
         <Route path="catalog" element={<Catalog />} />
+        <Route path="login" element={<Login />} />
         {/* <Route path="signup" element={<Signup />} /> */}
-        {/* <Route path="login" element={<Login />} /> */}
       </Route>
     </Routes>
   );
