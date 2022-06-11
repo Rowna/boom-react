84e9ac66f055b19af1af644ad997ccd55379fd15
Rowan Kh <rowan.kh1@gmail.com>
2022-06-11 11:18:23 +0200

    w: Refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
-import "./App.css";
+// import "./App.css";
 import "../src/main.scss";
 import { FirebaseContextProvider } from "./context/FirebaseContext";
-// import React, { useEffect } from "react";
-
-// import firebase_config from "./server/firebase_config";
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth"
-
 import { Route, Routes } from "react-router-dom";
 import Layout from "./containers/Layout";
 import Index from "./components/Index/Index";
@@ -16,6 +10,7 @@ import SingleView from "./components/SingleView/SingleView";
 import Signup from "./components/Signup/Signup";
 import Hero from "./components/Hero/Hero";
 import Cart from "./components/Cart/Cart";
+import Modal from "./containers/Modal";
 
 function App() {
   return (
@@ -24,19 +19,13 @@ function App() {
         {/* Interne Links definieren */}
         <Route element={<Layout />}>
           <Route index element={<Index />} />
-          <Route path="hero" element={<Hero />} />
+          <Route path="/" element={<Hero />} />
           <Route path="login" element={<Login />} />
           <Route path="signup" element={<Signup />} />
           <Route path="catalog" element={<Catalog />} />
-          {/* Im React-Router erkennt man eine Variable am ":Doppelpunkt" 
-              im 'path'-Prop von <Route>. ":artID" ist also eine Variable, 
-              und im <SingleView>-Component können wir mit useParams().artID
-              auf sie zugreifen.
-              Bei 'http://localhost:3000/singleview/FcXPA1SUVSEo1ckvNxmj'
-              ist 'FcXPA1SUVSEo1ckvNxmj' also der Wert von :artID.
-          */}
           <Route path="singleview/:artID" element={<SingleView />} />
           <Route path="cart" element={<Cart />} />
+          <Route path="modal" element={<Modal />} />
         </Route>
       </Routes>
     </FirebaseContextProvider>
@@ -45,3 +34,10 @@ function App() {
 
 export default App;
 // perl-variablen $name @names frz.: route -> de: Route  Kharnoub
+/* Im React-Router erkennt man eine Variable am ":Doppelpunkt" 
+    im 'path'-Prop von <Route>. ":artID" ist also eine Variable, 
+    und im <SingleView>-Component kann ich mit useParams().artID
+    auf sie zugreifen.
+    Bei 'http://localhost:3000/singleview/FcXPA1SUVSEo1ckvNxmj'
+    ist 'FcXPA1SUVSEo1ckvNxmj' also der Wert von :artID.
+*/
