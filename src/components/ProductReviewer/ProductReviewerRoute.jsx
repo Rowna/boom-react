cac1e6817ca189f129a5790c735920419f5bad8d
Rowan Kh <rowan.kh1@gmail.com>
2022-10-18 18:25:34 +0200

    w: productReviewerRoute erstellt ohne Carousel

--- src/components/ProductReviewer/ProductReviewerRoute.jsx
+++ src/components/ProductReviewer/ProductReviewerRoute.jsx
@@ -9,6 +9,7 @@ function ProductReviewerRoute() {
   let { artID } = useParams();
 
   const [item, setItem] = useState({});
+  // console.log(item.attributes);
 
   const getArticleById = (id) => {
     axios
@@ -17,7 +18,6 @@ function ProductReviewerRoute() {
       .then((res) => res.data)
       // response comming from the server
       .then((data) => {
-        // console.log("Images is  " + data.article.images[0]);
         // navigate("/singleitem");
         setItem(data.article);
       })
@@ -33,22 +33,75 @@ function ProductReviewerRoute() {
 
   return (
     <>
+      <div className="item-container_title">
+        <h2>BOOM</h2>
+        <p className="item-subtitle is-7">
+          Select favorite colors and sizes for ur Kids
+        </p>
+      </div>
+
       {item && (
-        <div className="item-container">
+        <div className="items-container box">
           {/* {item.title} */}
-          <div className="item-title">
-            <h2>BOOM</h2>
-            <p className="item-subtitle is-7">
-              Select favorite colors and sizes for ur Kids
-            </p>
+          <div className="images-container">
+            {item.images &&
+              item.images.map((image, id) => (
+                <figure className="image is-128x128">
+                  <img key={id} src={image && image} alt="article" />
+                </figure>
+              ))}
           </div>
-          {item.images &&
-            // item.images.map((image) => <SingleItemGallery images={image} />)}
-            item.images.map((image) => (
-              <figure>
-                <img src={image && image} alt="article" />
-              </figure>
-            ))}
+          <div className="images-info">
+            <div className="card-footer image-color">
+              <div className="card-footer-item color-div">
+                <h2 className="title is-4">Color </h2>
+              </div>
+
+              <div className="colors card-footer-item buttons are-medium">
+                {item.attributes &&
+                  item.attributes[0].colors.map((color, id) => (
+                    <button key={id} className="button is-normal color-btn">
+                      {color}
+                    </button>
+                  ))}
+              </div>
+            </div>
+
+            <div className="card-footer image-size">
+              <div className="card-footer-item size-div">
+                <h2 className="title is-4">Size </h2>
+              </div>
+
+              <div className="colors card-footer-item buttons are-medium">
+                {item.attributes &&
+                  item.attributes[0].sizes.map((size, id) => (
+                    <button key={id} className="button is-normal size-btn">
+                      {size} Years
+                    </button>
+                  ))}
+              </div>
+            </div>
+            <div className="card-footer images-btn">
+              <div className="card-footer-item">
+                <a href="/" className="button card-footer-item">
+                  add to Card
+                </a>
+              </div>
+              <div className="card-footer-item">
+                <a href="/" className="button card-footer-item">
+                  add to Favorite
+                </a>
+              </div>
+            </div>
+            {/*
+            <div className="card-footer image-color">
+              <div className="card-footer-item description-div">
+                <h2 className="title is-4">Article Description</h2>
+              </div>
+                 {item.title} 
+            </div>
+            */}
+          </div>
         </div>
       )}
     </>
